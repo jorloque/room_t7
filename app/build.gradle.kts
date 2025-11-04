a083c318a1898b0a4d4f121893675eac7944d2d3
Jorge Lopez <jorloque@gmail.com>
2025-11-04 21:10:43 +0100

    definimos TaskEntity.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/lopez_jorge/room_t7/TaskEntity.kt
@@ -0,0 +1,13 @@
+package com.lopez_jorge.room_t7
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    @ColumnInfo(name ="name" ) var name: String,
+    @ColumnInfo(name ="isDone" ) var isDone: Boolean = false
+)
